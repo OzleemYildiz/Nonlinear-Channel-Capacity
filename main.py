2e24d42a5f9974ee02c61f0ba710aa0036867ea1
OzleemYildiz <zy2043@nyu.edu>
2025-01-02 23:28:04 -0500

    name fixed

--- main.py
+++ main.py
@@ -58,7 +58,6 @@ def define_save_location(config):
 
     save_location = save_location + (
         "/"
-        + "G"
         + config["cons_str"]
         + "_phi="
         + str(config["nonlinearity"])
