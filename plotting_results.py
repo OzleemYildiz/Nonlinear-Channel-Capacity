9f7ee0c97da88932e85584e2b8c697f75da13576
OzleemYildiz <zy2043@nyu.edu>
2025-02-13 16:13:12 -0500

    sigma_1=8

--- plotting_results.py
+++ plotting_results.py
@@ -7,6 +7,126 @@ import pandas as pd
 import seaborn as sns
 
 
+def distribution_plot():
+    regime = 3
+    k = 4
+    phi = [0, 3, 5]
+    name = []
+    filepath = "Paper_Figure/Distribution/"
+
+    name.append(
+        "_Avg_phi=0_regime=3_min_samples=64_tdm=False_lr=[0.0001]_sigma1=1_sigma2=1_gd=True"
+    )
+    name.append(
+        "_Avg_phi=3_regime=3_min_samples=64_tdm=False_lr=[0.0001]_sigma1=1_sigma2=1_gd=True_4tanh0.25x"
+    )
+    name.append(
+        "_Avg_phi=5_regime=3_min_samples=64_tdm=False_lr=[0.0001]_sigma1=1_sigma2=1_gd=True_clipx=4_clipy=4"
+    )
+    fig1, ax1 = plt.subplots(figsize=(5, 4), tight_layout=True)
+    pdf = []
+    for ind, name_i in enumerate(name):
+        data = io.loadmat(filepath + name_i + "/res.mat")
+        power_change = data["Power_Change"]
+        pdf.append(io.loadmat(filepath + name_i + "/pdf.mat"))
+        ax1.plot(
+            power_change[0],
+            data["Learned_Capacity"][0],
+            label="$\phi=$" + str(phi[ind]),
+            linewidth=2,
+        )
+    ax1.set_xlabel("$P_1$", fontsize=10)
+    ax1.set_ylabel("$R_L$", fontsize=10)
+    ax1.legend(loc="best", fontsize=10)
+    ax1.grid(
+        visible=True,
+        which="major",
+        axis="both",
+        color="lightgray",
+        linestyle="-",
+        linewidth=0.5,
+    )
+    plt.minorticks_on()
+    ax1.grid(
+        visible=True,
+        which="minor",
+        axis="both",
+        color="gainsboro",
+        linestyle=":",
+        linewidth=0.5,
+    )
+    ax1.set_title("$k=$" + str(k))
+    fig1.savefig(filepath + "comp_rate.png")
+    plt.close()
+
+    config = {
+        "min_samples": 64,
+        "regime": 3,
+        "cons_type": 1,
+        "sigma_1": 200,
+        "sigma_2": 200,
+        "nonlinearity": 3,
+        "tanh_factor": 200,
+        "stop_sd": 4,
+    }
+
+    for ind, p in enumerate(power_change[0]):
+        alphabet_x, _, max_x, _ = get_alphabet_x_y(config, p, k)
+        alphabet_x = alphabet_x.numpy()
+        alphabet_x_boundaries = alphabet_x[1:] - (alphabet_x[1] - alphabet_x[0]) / 2
+
+        data = {}
+        fig, ax2 = plt.subplots(figsize=(5, 4), tight_layout=True)
+        w = 1
+        for j in range(len(name)):
+            try:
+                pdf_x, alph_x = pdf[j]["Chng" + str(int(p * 10))]
+            except:
+                pdf_x, alph_x = pdf[j]["Chng" + str(int(p * 10 - 1))]
+
+            # to make sure that comparison is fair with the same alphabet
+            indices = np.digitize(alph_x, alphabet_x_boundaries)
+            pdf_x_update = np.array(
+                [pdf_x[indices == i].sum() for i in range(0, len(alphabet_x))]
+            )
+            pdf_x_update = pdf_x_update * 20000
+            data = np.repeat(alphabet_x, pdf_x_update.astype(int))
+
+            # sns.displot(data, multiple="stack", kind="kde") - this is figure plot, i dont know how to stack, maybe pandas first
+            sns.histplot(
+                data,
+                bins=70,
+                kde=True,
+                ax=ax2,
+                stat="probability",
+                label="$\phi$=" + str(phi[j]),
+            )
+
+        ax2.set_xlabel("$x$", fontsize=10)
+        ax2.set_ylabel("$p_x(x)$", fontsize=10)
+        ax2.legend(loc="best", fontsize=10)
+        ax2.grid(
+            visible=True,
+            which="major",
+            axis="both",
+            color="lightgray",
+            linestyle="-",
+            linewidth=0.5,
+        )
+        plt.minorticks_on()
+        ax2.grid(
+            visible=True,
+            which="minor",
+            axis="both",
+            color="gainsboro",
+            linestyle=":",
+            linewidth=0.5,
+        )
+        ax2.set_title("$P_1=$" + str(round(p, 2)) + ", $k=$" + str(k))
+        fig.savefig(filepath + "pdf_" + str(int(p * 10)) + ".png")
+        plt.close()
+
+
 # Plotting the effect of sigma on the performance of the algorithm
 def sigma_affect_plot():
     filepath = "Paper_Figure/Sigma_Affect/"
@@ -177,7 +297,8 @@ def plot_pdf_x():
 
 def main():
     plt.rcParams["text.usetex"] = True
-    sigma_affect_plot()
+    # sigma_affect_plot()
+    distribution_plot()
 
 
 if __name__ == "__main__":
